bc02c6cd1dcbbe78f1b6a70aec6c2e29269d8ff5
Oliver Edwards <oedwards@editure.co.uk>
2010-10-26 10:54:24 +0100

    better url params

--- js/db.js
+++ js/db.js
@@ -117,6 +117,25 @@ jQuery.extend(db, {
 						return acc / len;
 				},
 
+				getUrlParams : function() {
+						var vars = {}, hash;
+								var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+								for(var i = hashes.length-1; i >= 0; i--)
+								{
+										hash = hashes[i].split('=');
+
+										var val = hash[1];
+										var intVal = parseInt(val);
+									 if (!isNaN(intVal)) {
+												val = intVal;
+										}else if (val==="true" || val==="false") {
+												val = Boolean(val);
+										}
+										vars[hash[0]] = val;
+								}
+								return vars;
+				},
+
 				HookStore : function(hooks, globalArgs) {
 
 						globalArgs = globalArgs || {};
